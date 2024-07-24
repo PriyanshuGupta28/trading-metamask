a3ea261bfdee018378d49b868a3e7c8bd0ab1416
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-07-24 23:19:44 +0530

    Create News.tsx

---
+++ frontend/src/pages/News/News.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Lottie from 'lottie-react'
+import commingsoon from '../../assets/comming.json'
+
+const News: React.FC = () => {
+    return (
+        <Lottie animationData={commingsoon}></Lottie>
+    )
+}
+
+export default News
