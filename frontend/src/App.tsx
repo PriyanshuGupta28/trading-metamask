0efe04b293d513ab481154a9789e344f709404f6
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-07-24 23:18:53 +0530

    ± lottie animated data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
+import { createTheme, ThemeProvider } from '@mui/material'
 import './App.css'
 import AllRoutes from './components/AllRoutes/AllRoutes'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import { rootColors } from './Utilities/rootColors'
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
   return (
-    <>
-      <AllRoutes />
-    </>
+    <div style={{ background: rootColors?.bgColor, color: rootColors?.text }}>
+      <ThemeProvider theme={darkTheme}>
+        <Navbar />
+        <AllRoutes />
+        <Footer />
+      </ThemeProvider>
+    </div>
   )
 }
 
