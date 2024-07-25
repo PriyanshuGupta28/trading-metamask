2d1d0a1071ec8ad0d9f2b681353659e912e13bd5
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-07-25 18:07:59 +0530

    Update UserWithdraw.tsx

--- frontend/src/pages/UserWithdraw/UserWithdraw.tsx
+++ frontend/src/pages/UserWithdraw/UserWithdraw.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
-import { Stack } from '@mui/material'
+import { Grid, Stack } from '@mui/material'
+import { UserWithdrawCardData } from '../../Utilities/utilitiesData'
+import DetailsCard from '../../components/DetailsCard/DetailsCard'
 
 const UserWithdraw: React.FC = () => {
     return (
-        <Stack>UserWithdraw</Stack>
+        <Stack sx={{
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Stack sx={{ width: '95%', gap: { xs: 5, md: 3 } }}>
+                <Grid container spacing={2}>
+                    {UserWithdrawCardData?.map((item, index) => (
+                        <Grid item xs={12} md={3}>
+                            <DetailsCard key={index} fund={item?.fund} type={item?.type} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Stack>
+        </Stack>
     )
 }
 
