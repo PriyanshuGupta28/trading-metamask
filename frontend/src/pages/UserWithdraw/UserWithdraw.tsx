58195cf720e87e7059711fc2ae9ffc6b99e40e40
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-07-25 11:30:26 +0530

    Create UserWithdraw.tsx

---
+++ frontend/src/pages/UserWithdraw/UserWithdraw.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+
+const UserWithdraw: React.FC = () => {
+    return (
+        <Stack>UserWithdraw</Stack>
+    )
+}
+
+export default UserWithdraw
