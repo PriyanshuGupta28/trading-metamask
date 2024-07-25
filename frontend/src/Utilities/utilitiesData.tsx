4d71032d16f2458f4215a6ddb583580675e46375
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-07-25 18:07:38 +0530

    Create utilitiesData.tsx

---
+++ frontend/src/Utilities/utilitiesData.tsx
@@ -0,0 +1,57 @@
+import { cardDetailsTypes } from "./types";
+
+export const CardData: cardDetailsTypes[] = [
+    {
+        fund: '$53,642,630.05',
+        type: 'Assets Under Management'
+    },
+    {
+        fund: '$53,642,630.05',
+        type: 'Assets Under Management'
+    },
+    {
+        fund: '$53,642,630.05',
+        type: 'Assets Under Management'
+    },
+    {
+        fund: '$53,642,630.05',
+        type: 'Assets Under Management'
+    },
+]
+
+export const UserDepositCardData: cardDetailsTypes[] = [
+    {
+        fund: '100',
+        type: "Account Balance"
+    },
+    {
+        fund: '200',
+        type: "Total Deposited"
+    },
+    {
+        fund: '300',
+        type: "Monthly Returns"
+    },
+    {
+        fund: '400',
+        type: "Total Returns"
+    },
+]
+export const UserWithdrawCardData: cardDetailsTypes[] = [
+    {
+        fund: '100',
+        type: "Account Balance"
+    },
+    {
+        fund: '200',
+        type: "Total Withdraw"
+    },
+    {
+        fund: '300',
+        type: "Pending Withdraw"
+    },
+    {
+        fund: '400',
+        type: "Total Fees"
+    },
+]
