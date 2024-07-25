5b207ccb3808dbb55fcafc8d0375499888d262c3
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-07-25 11:30:17 +0530

    Create UserDeposit.tsx

---
+++ frontend/src/pages/UserDeposit/UserDeposit.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+
+const UserDeposit: React.FC = () => {
+    return (
+        <Stack>UserDeposit</Stack>
+    )
+}
+
+export default UserDeposit
