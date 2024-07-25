1302603be48ec2b787e72729c95946f16df763c3
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-07-25 18:08:02 +0530

    Update UserDeposit.tsx

--- frontend/src/pages/UserDeposit/UserDeposit.tsx
+++ frontend/src/pages/UserDeposit/UserDeposit.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Stack } from '@mui/material'
+import { Grid, Stack } from '@mui/material'
+import DetailsCard from '../../components/DetailsCard/DetailsCard'
+import { UserDepositCardData } from '../../Utilities/utilitiesData'
 
 const UserDeposit: React.FC = () => {
     return (
-        <Stack>UserDeposit</Stack>
+        <Stack sx={{
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Stack sx={{ width: '95%', gap: { xs: 5, md: 3 } }}>
+                <Grid container spacing={2}>
+                    {UserDepositCardData?.map((item, index) => (
+                        <Grid item xs={12} md={3}>
+                            <DetailsCard key={index} fund={item?.fund} type={item?.type} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Stack>
+        </Stack>
     )
 }
 
-export default UserDeposit
+export default UserDeposit;
