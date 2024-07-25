085e9e3973ceb639d04fd7c803648ea621a23ddf
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-07-25 18:08:23 +0530

    Create DetailsCard.tsx

---
+++ frontend/src/components/DetailsCard/DetailsCard.tsx
@@ -0,0 +1,24 @@
+import { Stack, Typography } from '@mui/material'
+import React from 'react'
+import { rootColors } from '../../Utilities/rootColors'
+import { cardDetailsTypes } from '../../Utilities/types'
+
+const DetailsCard: React.FC<cardDetailsTypes> = ({ fund, type }) => {
+    return (
+        <Stack sx={{
+            justifyContent: 'center',
+            alignItems: 'start',
+            padding: '2rem 1.5rem',
+            gap: 1,
+            border: `1px solid ${rootColors?.text}`,
+            borderRadius: '0.75rem',
+            bgcolor: rootColors?.secondry
+
+        }}>
+            <Typography sx={{ whiteSpace: 'nowrap' }}>${fund} USD</Typography>
+            <Typography sx={{ whiteSpace: 'nowrap' }}>{type}</Typography>
+        </Stack>
+    )
+}
+
+export default DetailsCard
