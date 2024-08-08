1df40d32d913fb21691088b421aa7476f3a72088
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-08-08 18:53:29 +0530

    Create contractInstance.tsx

---
+++ frontend/src/contract/contractInstance.tsx
@@ -0,0 +1,24 @@
+import Web3 from 'web3';
+import InvestmentContract from "../../../blockchain/build/contracts/InvestmentContract.json";
+
+
+const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+
+const getContractInstance = async () => {
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = InvestmentContract.networks[networkId];
+
+    if (!deployedNetwork) {
+        throw new Error('Smart contract not deployed to detected network.');
+    }
+
+    const contractInstance = new web3.eth.Contract(
+        InvestmentContract.abi as any,
+        deployedNetwork && deployedNetwork.address
+    );
+
+    return contractInstance;
+};
+
+export default getContractInstance;
+
