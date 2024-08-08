b1737046d4dc34c840d6174e4906b5f3a2d54c09
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-08-08 18:55:40 +0530

    Revert "Update Home.tsx"
    This reverts commit fdd686705ba78ad1ce91cc19282d33d1d73d8048.

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import HeroSection from '../../components/HeroSection/HeroSection'
 import { Grid, Stack, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
@@ -9,46 +9,10 @@ import Lottie from 'lottie-react'
 import Faq from '../../components/Faq/Faq'
 import Card from '../../components/Card/Card'
 import SocialMedia from '../../components/SocialMedia/SocialMedia'
-import getContractInstance from '../../contract/contractInstance';
-
-interface InvestmentData {
-    fund: string;
-    investmentType: string;
-}
-
+import { CardData } from '../../Utilities/utilitiesData'
+import Test from '../../components/Test/Test'
 
 const Home: React.FC = () => {
-    const [investmentCount, setInvestmentCount] = useState<number>(0);
-    const [investments, setInvestments] = useState<InvestmentData[]>([]);
-    const [error, setError] = useState<string | null>(null);
-
-    useEffect(() => {
-        const fetchInvestmentData = async () => {
-            try {
-                const contractInstance = await getContractInstance();
-                const count = await contractInstance.methods.getInvestmentCount().call();
-                setInvestmentCount(Number(count));
-
-                const investmentPromises = [];
-                for (let i = 0; i < count; i++) {
-                    investmentPromises.push(contractInstance.methods.getInvestment(i).call());
-                }
-
-                const investmentsData = await Promise.all(investmentPromises);
-                const formattedInvestments = investmentsData.map((investment: [string, string]) => ({
-                    fund: investment[0],
-                    investmentType: investment[1],
-                }));
-                setInvestments(formattedInvestments);
-            } catch (err) {
-                setError(err.message);
-            }
-        };
-
-        fetchInvestmentData();
-    }, []);
-
-    console.log(investments, "investments")
     return (
         <Stack sx={{
             justifyContent: 'center',
@@ -57,9 +21,9 @@ const Home: React.FC = () => {
             <Stack sx={{ width: '90%', gap: { xs: 5, md: 3 } }}>
                 <HeroSection />
                 <Grid container spacing={2}>
-                    {investments?.map((item, index) => (
+                    {CardData?.map((item, index) => (
                         <Grid item xs={12} md={3}>
-                            <Card key={index} fund={item?.fund} type={item?.investmentType} />
+                            <Card key={index} fund={item?.fund} type={item?.type} />
                         </Grid>
                     ))}
                 </Grid>
@@ -165,6 +129,7 @@ const Home: React.FC = () => {
                 <SocialMedia />
                 <Faq />
             </Stack>
+            <Test />
         </Stack>
     )
 }
