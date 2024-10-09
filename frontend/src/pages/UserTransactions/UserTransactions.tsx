bccbfc90a7255c66d6734f514febf5b742755c4a
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-10-09 21:30:00 +0530

    Update UserTransactions.tsx

--- frontend/src/pages/UserTransactions/UserTransactions.tsx
+++ frontend/src/pages/UserTransactions/UserTransactions.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Stack } from '@mui/material'
+import UserTransactionsTable from '../../components/UserTransactionsTable/UserTransactionsTable'
 
 const UserTransactions: React.FC = () => {
     return (
-        <Stack>UserTransactions</Stack>
+        <Stack sx={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Stack width={'90%'}>
+                <UserTransactionsTable />
+            </Stack>
+        </Stack>
     )
 }
 
-export default UserTransactions
+export default UserTransactions;
