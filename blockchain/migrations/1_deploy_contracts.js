44c1112c749ccde812c50f93a9c41acf67f7c95f
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-10-09 21:30:14 +0530

    Update 1_deploy_contracts.js

--- blockchain/migrations/1_deploy_contracts.js
+++ blockchain/migrations/1_deploy_contracts.js
@@ -2,5 +2,6 @@ const InvestmentContract = artifacts.require("InvestmentContract");
 
 module.exports = function (deployer) {
   deployer.deploy(InvestmentContract);
+  // deployer.deploy(MetaMask);
   // deployer.link(InvestmentContract);
 };
