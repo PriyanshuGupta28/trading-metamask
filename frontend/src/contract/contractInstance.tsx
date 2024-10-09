e491a765321e3add66c76a83ab61ea7afd04d165
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-10-09 21:30:02 +0530

    Update contractInstance.tsx

--- frontend/src/contract/contractInstance.tsx
+++ frontend/src/contract/contractInstance.tsx
@@ -1,24 +1,22 @@
-import Web3 from 'web3';
+import Web3 from "web3";
 import InvestmentContract from "../../../blockchain/build/contracts/InvestmentContract.json";
 
-
-const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 
 const getContractInstance = async () => {
-    const networkId = (await web3.eth.net.getId()).toString();
-    const deployedNetwork = InvestmentContract.networks[networkId];
+  const networkId = (await web3.eth.net.getId()).toString();
+  const deployedNetwork = InvestmentContract.networks[networkId];
 
-    if (!deployedNetwork) {
-        throw new Error('Smart contract not deployed to detected network.');
-    }
+  if (!deployedNetwork) {
+    throw new Error("Smart contract not deployed to detected network.");
+  }
 
-    const contractInstance = new web3.eth.Contract(
-        InvestmentContract.abi as any,
-        deployedNetwork && deployedNetwork.address
-    );
+  const contractInstance = new web3.eth.Contract(
+    InvestmentContract.abi as any,
+    deployedNetwork && deployedNetwork.address
+  );
 
-    return contractInstance;
+  return contractInstance;
 };
 
 export default getContractInstance;
-
