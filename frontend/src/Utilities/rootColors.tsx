706ef83d184a2387a262355b6a5035a763fbe0b3
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-15 22:00:21 +0530

    Update rootColors.tsx

--- frontend/src/Utilities/rootColors.tsx
+++ frontend/src/Utilities/rootColors.tsx
@@ -1,8 +1,7 @@
 export const rootColors = {
-    bgColor: '#151A20',
-    primary: '#068DEF',
-    secondry: '#1D242E',
-    text: '#FFFEFE',
-    textSecondry: '#28333C'
-
-}
+  bgColor: "#151A20",
+  primary: "#068DEF",
+  secondry: "#1D242E",
+  text: "#FFFEFE",
+  textSecondry: "#28333C",
+};
