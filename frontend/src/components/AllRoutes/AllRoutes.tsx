b37af5b2d4c4631ca509b6d3326dfb779ac6a47e
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-15 21:59:30 +0530

    Update AllRoutes.tsx

--- frontend/src/components/AllRoutes/AllRoutes.tsx
+++ frontend/src/components/AllRoutes/AllRoutes.tsx
@@ -1,32 +1,21 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from '../../pages/Home/Home'
-import AboutUs from '../../pages/AboutUs/AboutUs'
-import ContactUs from '../../pages/ContactUs/ContactUs'
-import News from '../../pages/News/News'
-import Dapp from '../../pages/Dapp/Dapp'
-import Fund from '../../pages/Fund/Fund'
-import Error404 from '../Error404/Error404'
-import UserDeposit from '../../pages/UserDeposit/UserDeposit'
-import UserWithdraw from '../../pages/UserWithdraw/UserWithdraw'
-import UserTransactions from '../../pages/UserTransactions/UserTransactions'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Error404 from "../Error404/Error404";
+import UserDeposit from "../../pages/UserDeposit/UserDeposit";
+import UserWithdraw from "../../pages/UserWithdraw/UserWithdraw";
+import UserTransactions from "../../pages/UserTransactions/UserTransactions";
+import Dashboard from "../../pages/Dashboard/Dashboard";
 
 const AllRoutes: React.FC = () => {
-    return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/contact-us' element={<ContactUs />} />
-            <Route path='/about-us' element={<AboutUs />} />
-            <Route path='/news' element={<News />} />
-            <Route path='/dapp' element={<Dapp />} >
-                <Route path='fund' element={<Fund />} />
-                <Route path='deposit' element={<UserDeposit />} />
-                <Route path='withdraw' element={<UserWithdraw />} />
-                <Route path='transactions' element={<UserTransactions />} />
-            </Route>
-            <Route path='*' element={<Error404 />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/deposit" element={<UserDeposit />} />
+      <Route path="/withdraw" element={<UserWithdraw />} />
+      <Route path="/transactions" element={<UserTransactions />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
